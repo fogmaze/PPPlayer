fd73fef3d37db8bf0a4fb40660c62de7c4b7a875
yuenke <yuenskyke@gmail.com>
2023-04-19 18:56:58 +0800

    so fantastic

--- ball_detection/ColorRange.py
+++ ball_detection/ColorRange.py
@@ -1,51 +1,84 @@
 import cv2
 import numpy as np
 import pickle
+import os
 
-class ColorRange :
-    default:int
-    range:int
-
-    def __init__(self, default, range) :
-        self.default = default
-        self.range = range
-        
 def empty(a) :
     pass
 
-cam1 = cv2.VideoCapture(1)
+def save(path, object) :
+    with open(path, "wb") as f :
+        pickle.dump(object, f)
 
-cv2.namedWindow("ColorRange")
-cv2.resizeWindow("ColorRange", 640, 640)
+def load(path) :
+    if not os.path.exists(path) :
+        return ColorRange()
+    with open(path, "rb") as f :
+        return pickle.load(f)
 
-cv2.createTrackbar("Hue Min", "ColorRange", 0, 179, empty)
-cv2.createTrackbar("Hue Max", "ColorRange", 179, 179, empty)
-cv2.createTrackbar("Sat Min", "ColorRange", 0, 255, empty)
-cv2.createTrackbar("Sat Max", "ColorRange", 255, 255, empty)
-cv2.createTrackbar("Val Min", "ColorRange", 0, 255, empty)
-cv2.createTrackbar("Val Max", "ColorRange", 255, 255, empty)
+        
+class ColorRange :
+    upper:np.ndarray
+    lower:np.ndarray
+
+    def __init__(self) :
+        self.upper = np.array([179, 255, 255])
+        self.lower = np.array([0, 0, 0])
 
 
-while(True) :
-    ret1, rgb1 = cam1.read()
+    def getParameters(self, h_min, h_max, s_min, s_max, v_min, v_max) :
+        h_min = cv2.getTrackbarPos("Hue Min", "cam")
+        h_max = cv2.getTrackbarPos("Hue Max", "cam")
+        s_min = cv2.getTrackbarPos("Sat Min", "cam")
+        s_max = cv2.getTrackbarPos("Sat Max", "cam")
+        v_min = cv2.getTrackbarPos("Val Min", "cam")
+        v_max = cv2.getTrackbarPos("Val Max", "cam")
 
-    if ret1 :
-        hsv1 = cv2.cvtColor(rgb1, cv2.COLOR_BGR2HSV)
+        self.upper = np.array([h_max, s_max, v_max])
+        self.lower = np.array([h_min, s_min, v_min])
 
-        h_min = cv2.getTrackbarPos("Hue Min", "ColorRange")
-        h_max = cv2.getTrackbarPos("Hue Max", "ColorRange")
-        s_min = cv2.getTrackbarPos("Sat Min", "ColorRange")
-        s_max = cv2.getTrackbarPos("Sat Max", "ColorRange")
-        v_min = cv2.getTrackbarPos("Val Min", "ColorRange")
-        v_max = cv2.getTrackbarPos("Val Max", "ColorRange")
 
-        lower = np.array([h_min, s_min, v_min])
-        upper = np.array([h_max, s_max, v_max])
+    def run(self, cam1, cam2) :
+        cv2.namedWindow("cam")
+        cv2.resizeWindow("cam", 640, 640)
 
-        mask1 = cv2.inRange(hsv1, lower, upper)
+        cv2.createTrackbar("Hue Min", "cam", self.lower[0], 179, empty)
+        cv2.createTrackbar("Hue Max", "cam", self.upper[0], 179, empty)
+        cv2.createTrackbar("Sat Min", "cam", self.lower[1], 255, empty)
+        cv2.createTrackbar("Sat Max", "cam", self.upper[1], 255, empty)
+        cv2.createTrackbar("Val Min", "cam", self.lower[2], 255, empty)
+        cv2.createTrackbar("Val Max", "cam", self.upper[2], 255, empty)
 
-        cv2.imshow("Cam1", mask1)
+        while True :
+            ret1, frame1 = cam1.read()
+            ret2, frame2 = cam2.read()
 
-    cv2.waitKey(5)
+            if ret1 and ret2:
+                self.getParameters(self.lower[0], self.upper[0], self.lower[1], self.upper[1], self.lower[2], self.upper[2])
+                hsv1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
+                hsv2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2HSV)
+                mask1 = cv2.inRange(hsv1, self.lower, self.upper)
+                mask2 = cv2.inRange(hsv2, self.lower, self.upper)
+                cv2.imshow("Mask1", mask1, mask2)
+                cv2.imshow("Mask2", mask2)
+                cv2.imshow("cam1", frame1)
+                cv2.imshow("cam2", frame2)
+            else :
+                break
 
-cv2.destroyAllWindows()
+            key = cv2.waitKey(5)
+            if key == ord(" ") :
+                break
+            
+        cv2.destroyAllWindows()
+
+
+
+if __name__ == "__main__" :
+    cam1 = cv2.VideoCapture(1)
+    cam2 = cv2.VideoCapture(2)
+
+    cr = load("color_range")
+    
+    cr.run(cam1, cam2)
+    save("color_range", cr)
--- ball_detection/Dectection.py
+++ ball_detection/Dectection.py
@@ -1,3 +1,9 @@
 import cv2
 import numpy as np
+from ColorRange import *
 
+cam = cv2.VideoCapture(1)
+cr = load("color_range")
+    
+cr.run(cam)    
+save("color_range", cr)
