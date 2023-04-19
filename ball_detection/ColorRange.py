c22578581cbdbb14c26a64068098ec3bb3e2c34a
yuenke <yuenskyke@gmail.com>
2023-04-20 00:04:24 +0800

    debug something

--- ball_detection/ColorRange.py
+++ ball_detection/ColorRange.py
@@ -27,26 +27,26 @@ class ColorRange :
 
 
     def getParameters(self, h_min, h_max, s_min, s_max, v_min, v_max) :
-        h_min = cv2.getTrackbarPos("Hue Min", "cam")
-        h_max = cv2.getTrackbarPos("Hue Max", "cam")
-        s_min = cv2.getTrackbarPos("Sat Min", "cam")
-        s_max = cv2.getTrackbarPos("Sat Max", "cam")
-        v_min = cv2.getTrackbarPos("Val Min", "cam")
-        v_max = cv2.getTrackbarPos("Val Max", "cam")
+        h_min = cv2.getTrackbarPos("Hue Min", "ColorRangeSetting")
+        h_max = cv2.getTrackbarPos("Hue Max", "ColorRangeSetting")
+        s_min = cv2.getTrackbarPos("Sat Min", "ColorRangeSetting")
+        s_max = cv2.getTrackbarPos("Sat Max", "ColorRangeSetting")
+        v_min = cv2.getTrackbarPos("Val Min", "ColorRangeSetting")
+        v_max = cv2.getTrackbarPos("Val Max", "ColorRangeSetting")
 
         self.upper = np.array([h_max, s_max, v_max])
         self.lower = np.array([h_min, s_min, v_min])
 
 
     def run(self, cam1, cam2) :
-        cv2.namedWindow("cam")
+        cv2.namedWindow("ColorRangeSetting")
 
-        cv2.createTrackbar("Hue Min", "cam", self.lower[0], 179, empty)
-        cv2.createTrackbar("Hue Max", "cam", self.upper[0], 179, empty)
-        cv2.createTrackbar("Sat Min", "cam", self.lower[1], 255, empty)
-        cv2.createTrackbar("Sat Max", "cam", self.upper[1], 255, empty)
-        cv2.createTrackbar("Val Min", "cam", self.lower[2], 255, empty)
-        cv2.createTrackbar("Val Max", "cam", self.upper[2], 255, empty)
+        cv2.createTrackbar("Hue Min", "ColorRangeSetting", self.lower[0], 179, empty)
+        cv2.createTrackbar("Hue Max", "ColorRangeSetting", self.upper[0], 179, empty)
+        cv2.createTrackbar("Sat Min", "ColorRangeSetting", self.lower[1], 255, empty)
+        cv2.createTrackbar("Sat Max", "ColorRangeSetting", self.upper[1], 255, empty)
+        cv2.createTrackbar("Val Min", "ColorRangeSetting", self.lower[2], 255, empty)
+        cv2.createTrackbar("Val Max", "ColorRangeSetting", self.upper[2], 255, empty)
 
         while True :
             ret1, frame1 = cam1.read()
@@ -54,16 +54,23 @@ class ColorRange :
 
             if ret1 and ret2:
                 self.getParameters(self.lower[0], self.upper[0], self.lower[1], self.upper[1], self.lower[2], self.upper[2])
+
                 hsv1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
                 hsv2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2HSV)
                 mask1 = cv2.inRange(hsv1, self.lower, self.upper)
                 mask2 = cv2.inRange(hsv2, self.lower, self.upper)
 
+                cv2.putText(frame1, "Camera 1", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 128, 255), 2)
+                cv2.putText(frame2, "Camera 2", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 128, 255), 2)
+                cv2.putText(mask1, "Mask Camera 1", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 239, 97), 2)
+                cv2.putText(mask2, "Mask Camera 2", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 239, 97), 2)
+            
+
                 combined1 = cv2.hconcat([frame1, frame2])
                 combined2 = cv2.hconcat([mask1, mask2])
-                combined3 = cv2.vconcat([combined1, combined2])
                 
-                cv2.imshow("cam", combined3)
+                cv2.imshow("ColorRangeSetting", combined2)
+                cv2.imshow("ColorRangeMask", combined1)
             else :
                 break
 
@@ -76,8 +83,8 @@ class ColorRange :
 
 
 if __name__ == "__main__" :
-    cam1 = cv2.VideoCapture(1)
-    cam2 = cv2.VideoCapture(2)
+    cam1 = cv2.VideoCapture(0)
+    cam2 = cv2.VideoCapture(1)
 
     cr = load("color_range")
     
