4b42b9afedc9e46cd4271120cefef4e000f9e110
yuenke <yuenskyke@gmail.com>
2023-04-19 15:20:32 +0800

    it's good

--- ball_detection/ColorRange.py
+++ ball_detection/ColorRange.py
@@ -4,8 +4,8 @@ import numpy as np
 def empty(a) :
     pass
 
-cam1 = cv2.VideoCapture(0)
-cam2 = cv2.VideoCapture(1)
+cam1 = cv2.VideoCapture(1)
+#cam2 = cv2.VideoCapture(1)
 
 cv2.namedWindow("ColorRange")
 cv2.resizeWindow("ColorRange", 640, 640)
@@ -20,11 +20,11 @@ cv2.createTrackbar("Val Max", "ColorRange", 255, 255, empty)
 
 while(True) :
     ret1, rgb1 = cam1.read()
-    ret2, rgb2 = cam2.read()
+    #ret2, rgb2 = cam2.read()
 
-    if ret1 and ret2 :
+    if ret1 : #and ret2 :
         hsv1 = cv2.cvtColor(rgb1, cv2.COLOR_BGR2HSV)
-        hsv2 = cv2.cvtColor(rgb2, cv2.COLOR_BGR2HSV)
+        #hsv2 = cv2.cvtColor(rgb2, cv2.COLOR_BGR2HSV)
 
         h_min = cv2.getTrackbarPos("Hue Min", "ColorRange")
         h_max = cv2.getTrackbarPos("Hue Max", "ColorRange")
@@ -37,10 +37,11 @@ while(True) :
         upper = np.array([h_max, s_max, v_max])
 
         mask1 = cv2.inRange(hsv1, lower, upper)
-        mask2 = cv2.inRange(hsv2, lower, upper)
+        #mask2 = cv2.inRange(hsv2, lower, upper)
 
         cv2.imshow("Cam1", mask1)
-        cv2.imshow("Cam2", mask2)
+        #cv2.imshow("Cam2", mask2)
 
-        cv2.waitKey(1)
+    cv2.waitKey(10)
 
+cv2.destroyallWindow()
