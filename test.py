b2627a0edff630f049dfc42d163ee6a4460aa81c
yeet <andy160830@gmail.com>
2023-05-31 19:57:21 +0800

    get camera position and calculate ideal ball size and

--- test.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-a = cv2.imread("ball_detection/test1.png")
-b = cv2.imread("ball_detection/test1.png")
-a = cv2.cvtColor(a, cv2.COLOR_BGR2HSV_FULL)
-
-cv2.imshow("aa", cv2.bitwise_xor(a, b))
-cv2.waitKey(0)
